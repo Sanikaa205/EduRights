444716d27bd962832a41b563e7394384d7c91fe5
Sanika Umesh Shinde <ssanikaa205@gmail.com>
2026-01-03 20:10:44 +0530

    Resolve merge conflict in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,49 @@
-
-
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "@/pages/Index";
-import Dashboard from "@/pages/Dashboard";
-import Modules from "@/pages/Modules";
-import EducationalGames from "@/pages/EducationalGames/EducationalGames";
+
+import Index from "./pages/Index";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Modules from "./pages/Modules";
+import Quiz from "./pages/Quiz";
+import Resources from "./pages/Resources";
+import Feedback from "./pages/Feedback";
+import NotFound from "./pages/NotFound";
+
+import EducationalGames from "./pages/EducationalGames/EducationalGames";
 import BuildYourSchool from "./pages/EducationalGames/BuildYourSchool/BuildYourSchool";
-  
-import Feedback from "@/pages/Feedback";
-import Login from "@/pages/Login";
-import Register from "@/pages/Register";
-import Resources from "@/pages/Resources";
-import NotFound from "@/pages/NotFound";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/modules" element={<Modules />} />
-        <Route path="/games" element={<EducationalGames />} />
-        <Route path="/games/build-your-school"element={<BuildYourSchool />}/>
-        <Route path="/feedback" element={<Feedback />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/resources" element={<Resources />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/modules" element={<Modules />} />
+            <Route path="/games" element={<EducationalGames />} />
+            <Route
+              path="/games/build-your-school"
+              element={<BuildYourSchool />}
+            />
+            <Route path="/feedback" element={<Feedback />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/resources" element={<Resources />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
   );
 }
