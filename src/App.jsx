edff6273d34d4a6fd7adc127a5bdb2384531c543
Sanika Umesh Shinde <ssanikaa205@gmail.com>
2026-01-09 01:00:25 +0530

    app.jsx fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import BrokenStory from "./pages/EducationalGames/BrokenStory/BrokenStory";
 
-// Pages
 import Index from "./pages/Index";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -15,69 +15,59 @@ import Resources from "./pages/Resources";
 import Feedback from "./pages/Feedback";
 import NotFound from "./pages/NotFound";
 
-// Educational Games
 import EducationalGames from "./pages/EducationalGames/EducationalGames";
 import LegalHeroJourney from "@/pages/EducationalGames/LegalHeroJourney/LegalHeroJourney";
 import LevelOneHomeRights from "@/pages/EducationalGames/LegalHeroJourney/LevelOneHomeRights";
-import LevelTwoSchoolRights from "@/pages/EducationalGames/LegalHeroJourney/LevelTwoSchoolRights";
-import LevelThreeOnlineSafety from "@/pages/EducationalGames/LegalHeroJourney/LevelThreeOnlineSafety";
-import LevelFourWorkplace from "@/pages/EducationalGames/LegalHeroJourney/LevelFourWorkplace";
-import LevelUpAnimation from "@/pages/EducationalGames/LegalHeroJourney/LevelUpAnimation";
-import BrokenStory from "./pages/EducationalGames/BrokenStory/BrokenStory";
 import BuildYourSchool from "./pages/EducationalGames/BuildYourSchool/BuildYourSchool";
 
-// Module 1 Pages
 import Module1Start from "./pages/module1/Module1Start";
 import Module1Story from "./pages/module1/Module1Story";
 import Module1Explanation from "./pages/module1/Module1Explanation";
 import Module1Quiz from "./pages/module1/Module1Quiz";
-
-
+import Module2Start from "./pages/module2/Module2Start";
+import Module2Explanation from "./pages/module2/Module2Explanation";
+import Module2Quiz from "./pages/module2/Module2Quiz";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          {/* Main Pages */}
-          <Route path="/" element={<Index />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/modules" element={<Modules />} />
-          <Route path="/quiz" element={<Quiz />} />
-          <Route path="/resources" element={<Resources />} />
-          <Route path="/feedback" element={<Feedback />} />
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
 
-          {/* Educational Games */}
-          <Route path="/games" element={<EducationalGames />} />
-          <Route path="/games/legal-hero-journey" element={<LegalHeroJourney />} />
-          <Route path="/games/legal-hero-journey/level-1" element={<LevelOneHomeRights />} />
-          <Route path="/games/legal-hero-journey/level-2" element={<LevelTwoSchoolRights />} />
-          <Route path="/games/legal-hero-journey/level-up-animation" element={<LevelUpAnimation />} />
-          <Route path="/games/legal-hero-journey/level-3" element={<LevelThreeOnlineSafety />} />
-          <Route path="/games/legal-hero-journey/level-4" element={<LevelFourWorkplace />} />
-          <Route path="/games/broken-story" element={<BrokenStory />} />
-          <Route path="/games/build-your-school" element={<BuildYourSchool />} />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/modules" element={<Modules />} />
+            <Route path="/games" element={<EducationalGames />} />
+            <Route path="/games/legal-hero-journey" element={<LegalHeroJourney />} />
+            <Route path="/games/legal-hero-journey/level-1" element={<LevelOneHomeRights />} />  
+              
+            <Route
+              path="/games/build-your-school"
+              element={<BuildYourSchool />}
+            />
+            <Route path="/feedback" element={<Feedback />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/resources" element={<Resources />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/module-1" element={<Module1Start />} />
+            <Route path="/module-1/story" element={<Module1Story />} />
+            <Route path="/module-1/explanation" element={<Module1Explanation />} />
+            <Route path="/module-1/quiz" element={<Module1Quiz />} />
+            <Route path="/module-2" element={<Module2Start />} />
+            <Route path="/module-2/explanation" element={<Module2Explanation />} />
+            <Route path="/module-2/quiz" element={<Module2Quiz />} />
+             <Route path="/games/broken-story" element={<BrokenStory />}/>
 
-          {/* Module 1 Routes */}
-          <Route path="/module-1/start" element={<Module1Start />} />
-          <Route path="/module-1/story" element={<Module1Story />} />
-          <Route path="/module-1/explanation" element={<Module1Explanation />} />
-          <Route path="/module-1/quiz" element={<Module1Quiz />} />
 
-         
-
-          {/* Catch-all */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
